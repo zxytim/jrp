29271909222aa55f0a27b028234b2a32063c4d3f
Xinyu Zhou <zxytim@gmail.com>
2019-05-01 13:49:40 +0800

    add more items

--- driver.py
+++ driver.py
@@ -14,9 +14,28 @@ def main():
     rst = arxiv.query(query, sort_by='lastUpdatedDate', start=0, max_results=100)
     with db_session:
         for r in rst:
+            area = '|'.join([t['term'] for t in r['tags']])
+            print(r['id'], area, r['title'].replace('\n ', ''), r['authors'])
             if not ArxivPaper.exists(id=r['id']):
                 ArxivPaper(id=r['id'], guidislink=r['guidislink'],
-                           updated=dateutil.parser.parse(r['updated']), data=r)
+                           updated=dateutil.parser.parse(r['updated']), 
+                           published=dateutil.parser.parse(r['published']),
+                           title=r['title'].replace('\n ', ''),
+                           title_detail=r['title_detail'],
+                           summary=r['summary'].replace('\n', ' ').replace('   ', '\n'),
+                           summary_detail=r['summary_detail'],
+                           authors=r['authors'],
+                           author=r['author'],
+                           author_detail=r['author_detail'],
+                           arxiv_comment=str(r['arxiv_comment']),
+                           links=r['links'],
+                           arxiv_primary_category=r['arxiv_primary_category'],
+                           tags=r['tags'],
+                           pdf_url=r['pdf_url'],
+                           affiliation=r['affiliation'],
+                           journal_reference=str(r['journal_reference']),
+                           doi=str(r['doi']),
+                           data=r)
     
     
 if __name__ == '__main__':
--- jrp/db.py
+++ jrp/db.py
@@ -1,4 +1,4 @@
-from pony.orm import Database, Required, Optional, Set, PrimaryKey, Json
+from pony.orm import Database, Required, Optional, Set, PrimaryKey, Json, IntArray, StrArray
 from datetime import datetime
 from . import config
 
@@ -10,6 +10,29 @@ class ArxivPaper(db.Entity):
     id = PrimaryKey(str)
     guidislink = Optional(bool)
     updated = Optional(datetime)
+    published = Optional(datetime)
+
+    title = Optional(str)
+    title_detail = Optional(Json)
+
+    summary = Optional(str)
+    summary_detail = Optional(Json)
+
+    authors = Optional(StrArray)
+    author = Optional(str)
+    author_detail = Optional(Json)
+
+    arxiv_comment = Optional(str)
+    links = Optional(Json)
+    arxiv_primary_category = Optional(Json)
+
+    tags = Optional(Json)
+
+    pdf_url = Optional(str)
+    affiliation = Optional(str)
+    journal_reference = Optional(str)
+    doi = Optional(str)
+
     data = Optional(Json)
 
 
